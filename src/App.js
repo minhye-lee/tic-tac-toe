574c0098284d912f419ea39a2e197e3d00df08b3
minhye.lee <minhye.lee@navercorp.com>
2019-01-10 17:20:54 +0900

    express 연동 완료

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Game from './components/Game';
 import Status from './components/Status';
 import Board from './components/Board';
 
-const App =  () => {
-  return(
-    <div className="app">
-      <Game />
-      <Board />
-      <Status />
-    </div>
-  );
+class App extends Component {
+  state = { username: null };
+  componentDidMount() {
+    fetch('/api/getUsername')
+    .then(res => res.json())
+    .then(user => this.setState({ username: user.username }));
+}
+
+  render() {
+    const { username } = this.state;
+    return(
+      <div className="app">
+        <Game />
+        <Board />
+        <Status />
+        <div>
+              {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
